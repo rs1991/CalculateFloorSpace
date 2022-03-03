0e2659a04de376a21e6959a62c665aef7f77b6fb
Rida Serroukh <rida.serroukh@gmail.com>
2022-03-03 12:25:37 +0000

    This is my latest attempt at the exercise.

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,8 +6,18 @@ namespace ConsoleApp1 // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
-            Console.WriteLine("line2");
+            double length, width, unit_price, total_price;
+            Console.WriteLine(" Please enter the length : ");
+            length = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Please enter the width : ");
+            width = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Enter the cost per unit : ");
+            unit_price = Convert.ToDouble(Console.ReadLine());
+
+            total_price = length * width * unit_price;
+            Console.WriteLine("The total cost is: " + total_price);
+            Console.ReadKey();
+            //This is my latest attempt
         }
     }
 }
