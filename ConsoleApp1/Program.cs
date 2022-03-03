9f8ef51dcb2bf878c8cdc8cf29082ae684c9a52e
Rida Serroukh <rida.serroukh@gmail.com>
2022-03-03 21:10:06 +0000

    Total area can now be calculated

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -6,7 +6,9 @@ namespace ConsoleApp1 // Note: actual namespace depends on the project name.
     {
         static void Main(string[] args)
         {
-            double length, width, unit_price, total_price;
+
+            //Cost to cover specified area by length and width
+            double length, width, unit_price, total_price, total_area, hourly_labour = 86.00;
             Console.WriteLine(" Please enter the length : ");
             length = Convert.ToDouble(Console.ReadLine());
             Console.WriteLine("Please enter the width : ");
@@ -16,8 +18,14 @@ namespace ConsoleApp1 // Note: actual namespace depends on the project name.
 
             total_price = length * width * unit_price;
             Console.WriteLine("The total cost is: " + total_price);
+           
+            total_area = length * width;
+            Console.WriteLine("The total area is: " + total_area);
             Console.ReadKey();
-            //This is my latest attempt
+
+
+            
+
         }
     }
 }
