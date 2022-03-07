09644cf0e9214c41242169a770f953b3c3fab4b3
Rida Serroukh <rida.serroukh@gmail.com>
2022-03-07 21:49:06 +0000

    Duplication removed for total price

--- CalculateFloorSpace/Program.cs
+++ CalculateFloorSpace/Program.cs
@@ -1,29 +1,26 @@
 ﻿using System;
 
-namespace CalculateFloorSpace 
+namespace CalculateFloorSpace
 {
     internal class Program
     {
+   
         static void Main(string[] args)
         {
 
             //Cost to cover specified area by length and width
-            double length,
-                width,
-                unit_price,
-                total_price,
-                total_area,
-                hourly_cost = 86,
-                maximum_flooring_per_hour = 20,
-                material_costs,
-                pi = Math.PI,
+            double length, width, unit_price, total_price=0, total_area,
+                hourly_cost = 86, maximum_flooring_per_hour = 20, material_costs, pi = Math.PI,
                 radius, triangle_base, triangle_height;
+                
+            string floor_type;    
+
+
+            Console.WriteLine("Please choose your floor type: 'c' circle, 'r' rectangle, 't' triangle: ");
+            floor_type = Console.ReadLine();
             
-            Console.WriteLine("Choose floor type: circle, wl, triangle: ");
-            String floor_type = Console.ReadLine();
 
-
-            if (floor_type == "wl")
+            if (floor_type == "r")
             {
                 Console.WriteLine(" Please enter the length : ");
                 length = Convert.ToDouble(Console.ReadLine());
@@ -35,10 +32,9 @@ namespace CalculateFloorSpace
                 total_area = length * width;
                 material_costs = unit_price * total_area;
                 total_price = total_area / maximum_flooring_per_hour * hourly_cost + material_costs;
-                Console.WriteLine("The total price is $: " + total_price);
-                Console.ReadKey();
             }
-            if (floor_type == "circle")
+
+            if (floor_type == "c")
             {
                 Console.WriteLine(" Please enter the radius: ");
                 radius = Convert.ToDouble(Console.ReadLine());
@@ -47,12 +43,10 @@ namespace CalculateFloorSpace
 
                 total_area = pi * radius * radius;
                 material_costs = unit_price * total_area;
-                total_price = total_area / maximum_flooring_per_hour * hourly_cost + material_costs;
-                Console.WriteLine("The total price is $: " + total_price);
-                Console.ReadKey();
+                total_price = total_area / maximum_flooring_per_hour * hourly_cost + material_costs;    
             }
-            if (floor_type == "triangle")
-             { 
+            if (floor_type == "t")
+            {
                 Console.WriteLine("Please enter the base");
                 triangle_base = Convert.ToDouble(Console.ReadLine());
                 Console.WriteLine("Please enter the height");
@@ -63,11 +57,10 @@ namespace CalculateFloorSpace
                 total_area = triangle_base * triangle_height / 2;
                 material_costs = unit_price * total_area;
                 total_price = total_area / maximum_flooring_per_hour * hourly_cost + material_costs;
-                Console.WriteLine("The total price is $: " + total_price);
-                Console.ReadKey();
 
             }
-
+            Console.WriteLine("Total price $ " + total_price);
+            Console.ReadKey();
         }
     }
 }
